0e4bad93567fefa316c17f7063131cfb7a6d613f
Michael Vezina <vezina95@gmail.com>
2022-07-18 10:07:20 -0400

    -update  api

--- mint_connector.py
+++ mint_connector.py
@@ -37,15 +37,15 @@ class MintConnector:
         )
 
     def get_transactions_df(self, include_investment=False, start_date=None, end_date=None):
+        # Max get_transaction_data limit is 100k
         transactions = self.mint.get_transaction_data(include_investment=include_investment, start_date=start_date,
-                                                      end_date=end_date, limit=2000000)
-        transactions.loc[(transactions.transaction_type == 'debit'), 'transaction_type'] = 'Expense'
-        transactions.loc[(transactions.transaction_type == 'credit'), 'transaction_type'] = 'Income'
-        # transactions = transactions[transactions.category != 'credit card payment']
+                                                      end_date=end_date, limit=100000)
+        transactions = pd.json_normalize(transactions)
         return transactions
 
     def get_accounts_df(self):
-        accts = self.mint.get_account_data(limit=2000000)
+        # Max acct limit is 5k
+        accts = self.mint.get_account_data(limit=5000)
         df = pd.DataFrame(accts)
         df['insert_time'] = datetime.datetime.now()
         return df
